121a22aeba995850cc130fedaf700f904b16a8f5
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-26 00:48:22 +0500

    Addition - Database is added

---
+++ 2- Jetpack Compose Notes App/app/src/main/java/com/android/cleanarchitecturenotesapp/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,11 @@
+package com.android.cleanarchitecturenotesapp.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.android.cleanarchitecturenotesapp.feature_note.domain.model.Note
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+}
