f7d83d7a58afa922b5962a59f9285255881ec377
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-25 21:17:09 +0500

    Addition - Notes app project is created

---
+++ 2- Jetpack Compose Notes App/app/src/main/java/com/android/cleanarchitecturenotesapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.android.cleanarchitecturenotesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.cleanarchitecturenotesapp.ui.theme.CleanArchitectureNotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArchitectureNotesAppTheme {
+                
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CleanArchitectureNotesAppTheme {
+        Greeting("Android")
+    }
+}
--- Jetpack Compose Notes App/README.md
+++
@@ -1,3 +0,0 @@
-# Jetpack-Compose
-Clean Architecture Note App (MVVM / CRUD / Jetpack Compose) <br>
-Guide Link https://www.youtube.com/watch?v=8YPXv7xKh2w
