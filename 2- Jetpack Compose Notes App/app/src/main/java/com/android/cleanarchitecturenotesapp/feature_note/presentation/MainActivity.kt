4c11ecdea3e0c663f51b3d4c3ea23115a126d8f2
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-25 23:05:33 +0500

    Addition - Packages are created for notes app

--- 2- Jetpack Compose Notes App/app/src/main/java/com/android/cleanarchitecturenotesapp/feature_note/presentation/MainActivity.kt
+++ 2- Jetpack Compose Notes App/app/src/main/java/com/android/cleanarchitecturenotesapp/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.android.cleanarchitecturenotesapp
+package com.android.cleanarchitecturenotesapp.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchitectureNotesAppTheme {
-                
+
             }
         }
     }
