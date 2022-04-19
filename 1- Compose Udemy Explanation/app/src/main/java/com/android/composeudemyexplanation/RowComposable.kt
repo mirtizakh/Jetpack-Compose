abc71fb2b8332557ab3deaf71865c772fad50c09
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-04-20 02:17:29 +0500

    Addition - Row composable is added

--- 1- Compose Udemy Explanation/app/src/main/java/com/android/composeudemyexplanation/RowComposable.kt
+++ 1- Compose Udemy Explanation/app/src/main/java/com/android/composeudemyexplanation/RowComposable.kt
@@ -1,10 +1,12 @@
 package com.android.composeudemyexplanation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -16,16 +18,20 @@ fun RowComposable() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Row {
+        Row(horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             HorizontalColumnBar(Color.Magenta)
             HorizontalColumnBar(Color.Red)
             HorizontalColumnBar(Color.Blue)
+            HorizontalColumnBar(Color.Yellow)
+            HorizontalColumnBar(Color.Green)
         }
     }
 }
 
 @Composable
-fun HorizontalColumnBar(color : Color){
+fun HorizontalColumnBar(color: Color) {
     Surface(
         color = color,
         modifier = Modifier.size(60.dp, 600.dp)
